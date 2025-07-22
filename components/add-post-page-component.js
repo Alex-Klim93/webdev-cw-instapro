6540e57daf74dd997d43112931973222b936cfac
Alex-Klim93 <melkimelki79@gmail.com>
2025-07-22 23:40:31 +0300

    Add files via upload

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,23 +1,72 @@
+// Добавляем в начало файла add-post-page-component.js
+import { renderHeaderComponent } from './header-component.js'
+import { renderUploadImageComponent } from './upload-image-component.js'
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+  // Функция рендеринга страницы добавления поста
   const render = () => {
-    // @TODO: Реализовать страницу добавления поста
+    let imageUrl = '' // Переменная для хранения URL загруженного изображения
+
+    // HTML-разметка страницы
     const appHtml = `
     <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+      <div class="header-container"></div> <!-- Контейнер для шапки -->
+      <div class="form">
+        <h3 class="form-title">Добавить пост</h3> <!-- Заголовок формы -->
+        <div class="form-inputs">
+          <div class="upload-image-container"></div> <!-- Контейнер для загрузки изображения -->
+          <textarea 
+            id="description-input" 
+            class="input textarea" 
+            rows="4" 
+            placeholder="Описание изображения"
+          ></textarea> <!-- Поле для ввода описания -->
+          <button class="button" id="add-button">Добавить</button> <!-- Кнопка отправки -->
+        </div>
+      </div>
     </div>
-  `;
+    `
 
-    appEl.innerHTML = appHtml;
+    appEl.innerHTML = appHtml // Вставляем разметку в DOM
 
-    document.getElementById("add-button").addEventListener("click", () => {
+    // Рендерим шапку страницы
+    renderHeaderComponent({
+      element: document.querySelector('.header-container'),
+    })
+
+    // Инициализируем компонент загрузки изображения
+    renderUploadImageComponent({
+      element: document.querySelector('.upload-image-container'),
+      onImageUrlChange(newImageUrl) {
+        // Колбэк, вызываемый при изменении URL изображения
+        imageUrl = newImageUrl
+      },
+    })
+
+    // Обработчик клика по кнопке "Добавить"
+    document.getElementById('add-button').addEventListener('click', () => {
+      const description = document
+        .getElementById('description-input')
+        .value.trim()
+
+      // Валидация формы
+      if (!description) {
+        alert('Введите описание изображения')
+        return
+      }
+
+      if (!imageUrl) {
+        alert('Не выбрано изображение')
+        return
+      }
+
+      // Вызываем переданный колбэк с данными поста
       onAddPostClick({
-        description: "Описание картинки",
-        imageUrl: "https://image.png",
-      });
-    });
-  };
+        description,
+        imageUrl,
+      })
+    })
+  }
 
-  render();
+  render() // Первоначальный рендеринг страницы
 }
--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -1,109 +1,75 @@
-import { USER_POSTS_PAGE } from "../routes.js";
-import { renderHeaderComponent } from "./header-component.js";
-import { posts, goToPage } from "../index.js";
+import { USER_POSTS_PAGE } from '../routes.js'
+import { renderHeaderComponent } from './header-component.js'
+import { posts, goToPage, user } from '../index.js'
+import { likePost, dislikePost } from '../api.js'
 
 export function renderPostsPageComponent({ appEl }) {
-  // @TODO: реализовать рендер постов из api
-  console.log("Актуальный список постов:", posts);
+  const urlParams = new URLSearchParams(window.location.search)
+  const userId = urlParams.get('userId')
 
-  /**
-   * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
-   * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
-   */
+  let userName = ''
+  if (userId && posts.length > 0) {
+    const userPost = posts.find((post) => post.user.id === userId)
+    if (userPost) userName = userPost.user.name
+  }
+
   const appHtml = `
-              <div class="page-container">
-                <div class="header-container"></div>
-                <ul class="posts">
-                  <li class="post">
-                    <div class="post-header" data-user-id="642d00329b190443860c2f31">
-                        <img src="https://www.imgonline.com.ua/examples/bee-on-daisy.jpg" class="post-header__user-image">
-                        <p class="post-header__user-name">Иван Иваныч</p>
-                    </div>
-                    <div class="post-image-container">
-                      <img class="post-image" src="https://www.imgonline.com.ua/examples/bee-on-daisy.jpg">
-                    </div>
-                    <div class="post-likes">
-                      <button data-post-id="642d00579b190443860c2f32" class="like-button">
-                        <img src="./assets/images/like-active.svg">
-                      </button>
-                      <p class="post-likes-text">
-                        Нравится: <strong>2</strong>
-                      </p>
-                    </div>
-                    <p class="post-text">
-                      <span class="user-name">Иван Иваныч</span>
-                      Ромашка, ромашка...
-                    </p>
-                    <p class="post-date">
-                      19 минут назад
-                    </p>
-                  </li>
-                  <li class="post">
-                    <div class="post-header" data-user-id="6425602ce156b600f7858df2">
-                        <img src="https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680601502867-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-04-04%2520%25C3%2590%25C2%25B2%252014.04.29.png" class="post-header__user-image">
-                        <p class="post-header__user-name">Варварва Н.</p>
-                    </div>
-                  
-                    
-                    <div class="post-image-container">
-                      <img class="post-image" src="https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680670675451-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-03-31%2520%25C3%2590%25C2%25B2%252012.51.20.png">
-                    </div>
-                    <div class="post-likes">
-                      <button data-post-id="642cffed9b190443860c2f30" class="like-button">
-                        <img src="./assets/images/like-not-active.svg">
-                      </button>
-                      <p class="post-likes-text">
-                        Нравится: <strong>35</strong>
-                      </p>
-                    </div>
-                    <p class="post-text">
-                      <span class="user-name">Варварва Н.</span>
-                      Нарисовала картину, посмотрите какая красивая
-                    </p>
-                    <p class="post-date">
-                      3 часа назад
-                    </p>
-                  </li>
-                  <li class="post">
-                    <div class="post-header" data-user-id="6425602ce156b600f7858df2">
-                        <img src="https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680601502867-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-04-04%2520%25C3%2590%25C2%25B2%252014.04.29.png" class="post-header__user-image">
-                        <p class="post-header__user-name">Варварва Н.</p>
-                    </div>
-                  
-                    
-                    <div class="post-image-container">
-                      <img class="post-image" src="https://leonardo.osnova.io/97a160ca-76b6-5cba-87c6-84ef29136bb3/">
-                    </div>
-                    <div class="post-likes">
-                      <button data-post-id="642cf82e9b190443860c2f2b" class="like-button">
-                        <img src="./assets/images/like-not-active.svg">
-                      </button>
-                      <p class="post-likes-text">
-                        Нравится: <strong>0</strong>
-                      </p>
-                    </div>
-                    <p class="post-text">
-                      <span class="user-name">Варварва Н.</span>
-                      Голова
-                    </p>
-                    <p class="post-date">
-                      8 дней назад
-                    </p>
-                  </li>
-                </ul>
-              </div>`;
+    <div class="page-container">
+      <div class="header-container"></div>
+      ${userId ? `<h2 class="user-posts-title">Посты пользователя ${userName}</h2>` : ''}
+      <ul class="posts">
+        ${posts
+          .map(
+            (post) => `
+          <li class="post">
+            <!-- остальная разметка поста -->
+          </li>
+        `
+          )
+          .join('')}
+      </ul>
+    </div>
+  `
 
-  appEl.innerHTML = appHtml;
+  appEl.innerHTML = appHtml
 
   renderHeaderComponent({
-    element: document.querySelector(".header-container"),
-  });
+    element: document.querySelector('.header-container'),
+  })
 
-  for (let userEl of document.querySelectorAll(".post-header")) {
-    userEl.addEventListener("click", () => {
-      goToPage(USER_POSTS_PAGE, {
-        userId: userEl.dataset.userId,
-      });
-    });
-  }
+  // Обработчики кликов
+  document.querySelectorAll('.post-header').forEach((userEl) => {
+    userEl.addEventListener('click', () => {
+      goToPage(USER_POSTS_PAGE, { userId: userEl.dataset.userId })
+    })
+  })
+
+  document.querySelectorAll('.like-button').forEach((likeButton) => {
+    likeButton.addEventListener('click', () => {
+      if (!user) {
+        alert('Для лайков нужно авторизоваться')
+        goToPage(AUTH_PAGE)
+        return
+      }
+
+      const token = user ? `Bearer ${user.token}` : undefined
+      const postId = likeButton.dataset.postId
+      const post = posts.find((p) => p.id === postId)
+
+      const apiCall = post.isLiked ? dislikePost : likePost
+
+      apiCall({ token, postId })
+        .then((response) => {
+          posts = posts.map((p) => (p.id === postId ? response.post : p))
+          renderPostsPageComponent({ appEl }) // Ре-рендерим текущую страницу
+        })
+        .catch((error) => {
+          console.error(
+            `Ошибка ${post.isLiked ? 'снятия' : 'установки'} лайка:`,
+            error
+          )
+          alert(error.message)
+        })
+    })
+  })
 }
